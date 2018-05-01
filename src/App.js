2b6bbaf8dba439e8097a27ac7a3aa7dc2bff588e
PatiKoszi <patrykkosznik@gmail.com>
2018-05-01 12:42:43 +0200

    function outside the render and returned by this inside the render{return

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    const name = "Patryk";
+  getVal() {
+    return "Patryk";
+  }
 
+  render() {
     return (
       <div>
+      <h1>It is {this.getVal()}</h1>
+        <h1> I is working!</h1>
       <h1> I is working!</h1>
-      <h1> I is working!</h1>
-    <h1>My name is {(
-      function () { return 3; }
-      )
-    ()
-      }</h1>
+
+
       </div>
 
     );
