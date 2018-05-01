70bcb924997bd223d6229dc9629cb5a1b1f1a25b
PatiKoszi <patrykkosznik@gmail.com>
2018-05-01 12:47:31 +0200

    constructor super()

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  getVal() {
-    return "Patryk";
+  constructor() {
+    super();
+    this.name = "Patryk";
+  }
+
+
+  zupa(val) {
+    return "Asia " + val;
   }
 
   render() {
     return (
       <div>
-      <h1>It is {this.getVal()}</h1>
-        <h1> I is working!</h1>
+      <h1>It is {this.zupa(13)}</h1>
+        <h1> I is working! {this.name}</h1>
       <h1> I is working!</h1>
 
 
