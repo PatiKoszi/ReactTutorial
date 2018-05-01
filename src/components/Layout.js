9781222e241c50f4749ee83e12f0f3d3718849a3
PatiKoszi <patrykkosznik@gmail.com>
2018-05-01 13:48:06 +0200

    state example only inside the Component we

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,9 +4,20 @@ import Footer from "./Footer";
 
 
 class Layout extends React.Component {
+  constructor () {
+    super();
+    this.state = {name: "Patryk"};
+  }
+
   render() {
+    setTimeout(() => {
+      this.setState({name: "Asia"});
+    }, 1000 );
+
+
      return (
        <div>
+         {this.state.name}
          <Header/>
          <Footer/>
        </div>
