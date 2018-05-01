3742da5963b17ab9ef207939e3e68b8e9dad3a7d
PatiKoszi <patrykkosznik@gmail.com>
2018-05-01 12:32:08 +0200

    inisial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    const name = "Patryk";
+    return (
+      <div>
+      <h1> I is working!</h1>
+      <h1> I is working!</h1>
+    <h1>My name is {name}</h1>
+      </div>
+
+    );
+  }
+}
+
+export default App;
