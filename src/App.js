dc389efe5613033b029bdd4ef18331855abd0844
PatiKoszi <patrykkosznik@gmail.com>
2018-05-01 13:05:21 +0200

    Header and Layout created

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import Layout from "./components/Layout";
 
 class App extends Component {
-  constructor() {
-    super();
-    this.name = "Patryk";
-  }
-
-
-  zupa(val) {
-    return "Asia " + val;
-  }
-
   render() {
     return (
       <div>
-      <h1>It is {this.zupa(13)}</h1>
-        <h1> I is working! {this.name}</h1>
-      <h1> I is working!</h1>
-
-
+        <Layout/>
       </div>
 
-    );
+    )
   }
 }
 
