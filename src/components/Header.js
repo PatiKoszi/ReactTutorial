2101deff574e59c5a2c03349c9642257029e2ee2
PatiKoszi <patrykkosznik@gmail.com>
2018-05-01 13:24:55 +0200

    Title inside the Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Title from "./Header/Title";
 
 class Header extends React.Component {
   render() {
     return (
-      <header>Header</header>
+      <div><Title/></div>
 
     )
   }
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import Header from "./Header";
+import Footer from "./Footer";
 
 
 class Layout extends React.Component {
   render() {
-    var list = [
-      <li/>,
-      <li/>,
-      <li/>,
-    ];
+     return (
+       <div>
+         <Header/>
+         <Footer/>
+       </div>
 
-    return (
-      <ol>
-        {list}
-      </ol>
 
     );
   }
